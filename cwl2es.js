a479650d6f448dc468e06f6bf1c739f1940af363
Miguel Garcia <miguel.garcia@mirai-advisory.com>
2023-01-16 10:33:36 +0100

    moving index name to variable

--- cwl2es.js
+++ cwl2es.js
@@ -64,9 +64,9 @@ function transform(payload) {
 
         // index name format: cwl-YYYY.MM.DD
         var indexName = [
-            'cwl-' + timestamp.getUTCFullYear(),              // year
-            ('0' + (timestamp.getUTCMonth() + 1)).slice(-2),  // month
-            ('0' + timestamp.getUTCDate()).slice(-2)          // day
+            index_prefix + '-' + timestamp.getUTCFullYear(),              // year
+            ('0' + (timestamp.getUTCMonth() + 1)).slice(-2),              // month
+            ('0' + timestamp.getUTCDate()).slice(-2)                      // day
         ].join('.');
 
         var source = buildSource(logEvent.message, logEvent.extractedFields);
